313f821c12d50139c990ff7c3d51df3c54bffb47
Nikolas Stavrou <nstavr04@ucy.ac.cy>
2020-10-31 13:30:18 +0200

    Starting HW3

---
+++ cmake-build-debug/CMakeFiles/HW3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW3.dir/main.c.obj"
+  "HW3.exe"
+  "HW3.exe.manifest"
+  "HW3.pdb"
+  "libHW3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
